21a88030d671d9120358f1c792b51d28d297421c
lucg124@hotmail.fr <luc.georges@spherea.com>
2017-09-11 06:19:41 +0200

    done, it works

--- clientTest/app.js
+++ clientTest/app.js
@@ -1,83 +1,66 @@
 #!/usr/bin/env node
 
-var amqp = require('amqp');
+var amqp = require('amqplib/callback_api');
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://mongodb:27017');
 
 var Climber = require ('./model');
 
-/*var climber = new Climber({ name: "Denis", gym: "Arkose Nation"});
+amqp.connect('amqp://rabbitmq', function(err, conn) {
+    console.log('Connected to rabbit');
+    conn.createChannel(function(err, ch) {
+	var q1 = 'fanBoard';
+	var q2 = 'fanBoard2';
+	var msg = '{"name": "Jim", "gym": "BS.4"}';
 
-climber.save(function(err) {
-    if (err)
-	console.log('err: %j', err);
-});*/
-
-/*
-console.log('Starting to connect to Rabbit MQ...');
-
-var connection = amqp.createConnection({ host: process.env.RABBITMQ_PORT_5672_TCP_ADDR }, {reconnect:false});
-
-console.log('Connection Created. Waiting for connection be ready...');
-
-// Wait for connection to become established.
-connection.on('ready', function () {
-    console.log('Connection ready for use. Connecting to "logs" exchange...');
-
-    connection.exchange('logs', {type: 'fanout', autoDelete: false}, function(exchange){
-	console.log('The "logs" exchange is now ready for use. Publishing message...');
-
-	var message = 'hello world @ ' + new Date();
-	exchange.publish('', message);
-
-	console.log('Published message: "' + message + '"');
-	// Allow 1 second for the message publishing to complete
-	setTimeout(function() {
-
-	    console.log('Disconnecting...');
-
-	    connection.disconnect();
-
-	    console.log('Disconnected. Exiting...');
-	}, 200);
-    });
-});
-*/
-
-console.log('Starting to connect to Rabbit MQ...');
-
-var connection = amqp.createConnection({ host: "rabbitmq" }, {reconnect:false});
-
-console.log('Connection Created. Waiting for connection be ready...');
-
-// Wait for connection to become established.
-connection.on('ready', function () {
-    console.log('Connection ready for use. Connecting to "logs" exchange...');
-
-    connection.exchange('logs', {type: 'fanout', autoDelete: false}, function(exchange){
-
-	var queueName = "queue1";
-	console.log('The "logs" exchange is now ready for use. Connecting to queue ' + queueName);
-
-	connection.queue(queueName, { autoDelete: false }, function(q){
-	    console.log('Connected to "' + queueName + '" queue. Waiting for queue to become ready');
-
-	    // Catch all messages
-	    q.bind('logs', '');
-
-	    //q.on('queueBindOk', function() {
-	    console.log('The queue is now ready for use. Subscribing for messages (Ctrl+c to disconnect)...');
-
-	    // Receive messages
-	    q.subscribe(function (message) {
-		console.log('Received message... ');
-		// Print messages to stdout
-		var buf = new Buffer(message.data);
-		console.log(buf.toString('utf-8'));
-	    });
-	    //});
+	ch.assertQueue(q1, {durable: true});
+	ch.assertQueue(q2, {durable: true});
+	// Note: on Node 6 Buffer.from(msg) should be used
+	ch.sendToQueue(q2, new Buffer(msg), {persistent: true});
+	console.log(" [x] Sent %s", msg);
+	ch.consume(q1, function (msg) {
+	    console.log(" [x] Received %s", msg.content.toString());
+	    conn.close();
 	});
     });
 });
 
+amqp.connect('amqp://rabbitmq', function(err, conn) {
+    console.log('Re-connected to rabbit');
+    conn.createChannel(function (err, ch) {
+	var q1 = 'fanBoard';
+	var q2 = 'fanBoard2';
+
+	ch.assertQueue(q1, {durable: true});
+	ch.assertQueue(q2, {durable: true});
+	ch.consume(q2, function (msg) {
+	    try {
+		var item = JSON.parse(msg.content.toString());
+	    }
+	    catch (e) {
+		console.log("Invalid message received");
+	    }
+	    if (typeof item === "object" && item !== null) {
+		console.log('item: %j', item);
+		var climber = new Climber(item);
+
+		climber.save(function(err) {
+		    if (err)
+			console.log('err: %j', err);
+		    else
+			console.log('climber saved ok');
+		});
+		Climber.find({}, function (err, climbers) {
+		    if (err)
+			console.log('err: %j', err);
+		    if (!err && climbers) {
+			ch.sendToQueue(q1, new Buffer(climbers.toString()), {persistent: true});
+			console.log('sent: %s', climbers.toString());
+		    }
+		    setTimeout(function () {conn.close(); process.exit(0);}, 5000);
+		});
+	    }
+	}, {noAck: true});
+    });
+});
--- node1/app.js
+++ node1/app.js
@@ -1,89 +1,50 @@
 #!/usr/bin/env node
 
-var amqp = require('amqp');
+var amqp = require('amqplib/callback_api');
 var mongoose = require('mongoose');
-
-// Solve healthcheck problem in docker-compose then remove this line
-setTimeout(function() { mongoose.connect('mongodb://mongodb:27017'); }, 10000);
-
 var Climber = require ('./model');
 
-/*var climber = new Climber({ name: "Denis", gym: "Arkose Nation"});
+// Solve healthcheck problem in docker-compose then remove this line
+setTimeout(function() {
 
-climber.save(function(err) {
-    if (err)
-	console.log('err: %j', err);
-});*/
+    mongoose.connect('mongodb://mongodb:27017');
+    amqp.connect('amqp://rabbitmq', function(err, conn) {
+	console.log('Connected to rabbit');
+	conn.createChannel(function(err, ch) {
+	    var q1 = 'fanBoard';
+	    var q2 = 'fanBoard2';
 
+	    ch.assertQueue(q1, {durable: true});
+	    ch.assertQueue(q2, {durable: true});
+	    ch.consume(q2, function(msg) {
+		try {
+		    var item = JSON.parse(msg.content.toString());
+		}
+	        catch (e) {
+		    console.log(" [x] Received %s", msg.content.toString());
+		}
+		if (typeof item === "object" && item !== null) {
+		    console.log('item: %j', item);
+		    var climber = new Climber(item);
 
-console.log('Starting to connect to Rabbit MQ...');
-
-var connection = amqp.createConnection({ host: "rabbitmq" }, {reconnect:false});
-
-console.log('Connection Created. Waiting for connection be ready...');
-
-// Wait for connection to become established.
-connection.on('ready', function () {
-    console.log('Connection ready for use. Connecting to "logs" exchange...');
-
-    connection.exchange('logs', {type: 'fanout', autoDelete: false}, function(exchange){
-	console.log('The "logs" exchange is now ready for use. Publishing message...');
-
-	var message = 'hello world @ ' + new Date();
-	exchange.publish('', message);
-
-	console.log('Published message: "' + message + '"');
-	// Allow 1 second for the message publishing to complete
-	setTimeout(function() {
-
-	    console.log('Disconnecting...');
-
-	    connection.disconnect();
-
-	    console.log('Disconnected. Exiting...');
-	}, 200);
-    });
-});
-
-/*
-if( process.argv.length < 3) {
-    console.error('Please provide a queue name to connect to');
-    return process.exit(1);
-}
-
-console.log('Starting to connect to Rabbit MQ...');
-
-var connection = amqp.createConnection({ host: process.env.RABBITMQ_PORT_5672_TCP_ADDR }, {reconnect:false});
-
-console.log('Connection Created. Waiting for connection be ready...');
-
-// Wait for connection to become established.
-connection.on('ready', function () {
-    console.log('Connection ready for use. Connecting to "logs" exchange...');
-
-    connection.exchange('logs', {type: 'fanout', autoDelete: false}, function(exchange){
-
-	var queueName = process.argv[2];
-	console.log('The "logs" exchange is now ready for use. Connecting to queue ' + queueName);
-
-	connection.queue(queueName, { autoDelete: false }, function(q){
-	    console.log('Connected to "' + queueName + '" queue. Waiting for queue to become ready');
-
-	    // Catch all messages
-	    q.bind('logs', '');
-
-	    //q.on('queueBindOk', function() {
-	    console.log('The queue is now ready for use. Subscribing for messages (Ctrl+c to disconnect)...');
-
-	    // Receive messages
-	    q.subscribe(function (message) {
-		console.log('Received message... ');
-		// Print messages to stdout
-		var buf = new Buffer(message.data);
-		console.log(buf.toString('utf-8'));
-	    });
-	    //});
+		    climber.save(function(err) {
+			if (err)
+			    console.log('err: %j', err);
+			else
+			    console.log('climber saved ok');
+		    });
+                    Climber.find({}, function (err, climbers) {
+		        if (err)
+			    console.log('err: %j', err);
+		        if (!err && climbers) {
+			    ch.sendToQueue(q1, new Buffer(climbers.toString()), {persistent: true});
+			    console.log('sent: %s', climbers.toString());
+			}
+			setTimeout(function () {conn.close(); process.exit(0);}, 5000);
+		    });
+		}
+	    }, {noAck: true});
 	});
     });
-});
-*/
+
+}, 20000);
